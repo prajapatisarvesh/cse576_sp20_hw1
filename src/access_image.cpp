05beb8ce4e384d68ca74c66be94be956dade1b1a
Sarvesh Prajapati <sarvesh101p@gmail.com>
2023-08-29 21:03:21 -0400

    submit-hw1

--- src/access_image.cpp
+++ src/access_image.cpp
@@ -8,8 +8,9 @@
 int pixel_address(const Image& im, int x, int y, int ch)
   {
   // TODO: calculate and return the index
+  return x+im.w*y+im.w*im.h*ch;
+  // NOT_IMPLEMENTED();
   
-  NOT_IMPLEMENTED();
   
   return 0;
   }
@@ -22,9 +23,11 @@ float get_clamped_pixel(const Image& im, int x, int y, int ch)
   {
   // TODO: clamp the coordinates and return the correct pixel value
   
-  NOT_IMPLEMENTED();
+  // NOT_IMPLEMENTED();
+  x = (((x >= im.w) ? im.w-1 : x) < 0) ? 0 : ((x >= im.w) ? im.w-1 : x);
+  y = (((y >= im.h) ? im.h-1 : y) < 0) ? 0 : ((y >= im.h) ? im.h-1 : y);
   
-  return 0.0;
+  return im.data[pixel_address(im, x, y, ch)];
   }
 
 
@@ -35,8 +38,12 @@ void set_pixel(Image& im, int x, int y, int c, float value)
   {
   // TODO: Only set the pixel to the value if it's inside the image
   
-  NOT_IMPLEMENTED();
-  
+    // NOT_IMPLEMENTED();
+    if(x < im.w && y < im.h && c < im.c) {
+      im.data[pixel_address(im, x, y, c)] = value;
+    } else {
+      return;
+    }
   }
 
 
@@ -54,6 +61,9 @@ void copy_image(Image& to, const Image& from)
   // TODO: populate the remaining fields in 'to' and copy the data
   // You might want to check how 'memcpy' function works
   
-  NOT_IMPLEMENTED();
+  // NOT_IMPLEMENTED();
+  to.w = from.w;
+  to.h = from.h;
+  memcpy(to.data, from.data, sizeof(float)*from.w*from.h*from.c);
   
   }
--- src/process_image.cpp
+++ src/process_image.cpp
@@ -18,7 +18,17 @@ Image rgb_to_grayscale(const Image& im)
   // TODO: calculate the pixels of 'gray'
   
   
-  NOT_IMPLEMENTED();
+  // NOT_IMPLEMENTED();
+  const float r_const = 0.299;
+  const float g_const = 0.587;
+  const float b_const = 0.114;
+  float change_val = 0.0f;
+  for(int i = 0; i < im.w; i++) {
+    for(int j = 0; j < im.h; j++) {
+      change_val = r_const * im(i, j, 0) + g_const* im(i, j, 1) + b_const * im(i, j, 2);
+      gray(i, j, 0) = change_val;
+    }
+  }
   
   return gray;
   }
@@ -54,7 +64,16 @@ void shift_image(Image& im, int c, float v)
   
   // TODO: shift all the pixels at the specified channel
   
-  NOT_IMPLEMENTED();
+  // NOT_IMPLEMENTED();
+  float pix = 0.0f;
+  for(int i = 0; i < im.w; i++) {
+    for (int j = 0; j < im.h; j++) {
+      pix = 0.0f;
+      pix = im(i, j, c);
+      pix += v;
+      im.set_pixel(i, j, c, pix);
+    }
+  }
   
   }
 
@@ -79,7 +98,14 @@ void clamp_image(Image& im)
   {
   // TODO: clamp all the pixels in all channel to be between 0 and 1
   
-  NOT_IMPLEMENTED();
+  // NOT_IMPLEMENTED();
+  for(int i = 0; i < im.w; i++) {
+    for(int j = 0; j < im.h; j++) {
+      im.set_pixel(i, j, 0, (im(i, j, 0) < 0 ? 0 : im(i, j, 0)) > 1.0 ? 1.0 : im(i, j, 0) < 0 ? 0 : im(i, j, 0));
+      im.set_pixel(i, j, 1, (im(i, j, 1) < 0 ? 0 : im(i, j, 1)) > 1.0 ? 1.0 : im(i, j, 1) < 0 ? 0 : im(i, j, 1));
+      im.set_pixel(i, j, 2, (im(i, j, 2) < 0 ? 0 : im(i, j, 2)) > 1.0 ? 1.0 : im(i, j, 2) < 0 ? 0 : im(i, j, 2));
+    }
+  }
   
   }
 
@@ -105,6 +131,57 @@ void rgb_to_hsv(Image& im)
   
   NOT_IMPLEMENTED();
   
+  for(int i = 0; i < im.w; i++) {
+    for(int j = 0; j < im.h; j++) {
+      float r = 0.0f;
+      float g = 0.0f;
+      float b = 0.0f;
+      float v = 0.0f;
+      float m = 0.0f;
+      float c = 0.0f;
+      float s = 0.0f;
+      float h = 0.0f;
+      r = im(i, j, 0);
+      g = im(i, j, 1);
+      b = im(i, j, 2);
+      if(r > g) {
+        v = r;
+      } else {
+        v = g;
+      }
+      if(v < b) {
+        v = b;
+      }
+      if(r < g) {
+        m = r;
+      } else {
+        m = g;
+      }
+      if(m > b) {
+        m = b;
+      }
+      c = v-m;
+      s = (v == 0) ? 0 : c/v;
+      if(c == 0) {
+        h == 0;
+      } else if(v == r) {
+        h =   (g-b) / c;
+      } else if (v == g) {
+        h = (b-r) / c + 2;
+      } else if (v == b) {
+        h = (r-g) / c + 4;
+      }
+      if(h < 0) {
+        h = h/6 + 1;
+      } else {
+        h = h/6;
+      }
+      im(i, j, 0) = h;
+      im(i, j, 1) = s;
+      im(i, j, 2) = v;
+    }
+  }
+  
   }
 
 // HW0 #7
@@ -115,7 +192,47 @@ void hsv_to_rgb(Image& im)
   
   // TODO: Convert all pixels from HSV format to RGB format
   
-  NOT_IMPLEMENTED();
+  // NOT_IMPLEMENTED();
+  for(int i = 0; i < im.w; i++) {
+    for(int j = 0; j < im.h; j++) {
+      
+      float h = im(i, j, 0);
+      float s = im(i, j, 1);
+      float v = im(i, j, 2);
+      float c = v * s;
+      
+      float x = c * (1 - fabs((fmod(6 *h, 2) - 1)));
+      float m =   v - c;
+      float r = 0.0f;
+      float g = 0.0f;
+      float b = 0.0f;
+      if(h >= 0.0f && h < 1.0f/6.0f) {
+        r = c;
+        g = x;
+      } else if (h >= 1.0f/6.0f && h < 2.0f/6.0f) {
+        r = x;
+        g = c;
+      } else if (h >= 2.0f/6.0f && h < 3.0f/6.0f) {
+        g = c;
+        b = x;
+      } else if (h >= 3.0f/6.0f && h < 4.0f/6.0f) {
+        g = x;
+        b = c;
+      } else if (h >= 4.0f/6.0f && h < 5.0f/6.0f) {
+        r = x;
+        b = c;
+      } else if (h >= 5.0f/6.0f && h < 1.0f) {
+        r = c;
+        b = x;
+      } 
+      r += m;
+      g += m;
+      b += m;
+      im(i, j, 0) = r;
+      im(i, j, 1) = g;
+      im(i, j, 2) = b;
+    }
+  }
   
   }
 
